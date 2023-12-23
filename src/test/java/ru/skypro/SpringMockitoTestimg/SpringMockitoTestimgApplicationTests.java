50ada582327ebd917c4b91b9301ccb0ea5e3e217
Timofey <timofey.sharipov@gmail.com>
2023-12-24 02:50:48 +0300

    Initial commit

---
+++ src/test/java/ru/skypro/SpringMockitoTestimg/SpringMockitoTestimgApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.SpringMockitoTestimg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMockitoTestimgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
